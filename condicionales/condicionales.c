649b413abf72aafeca03885fb3c70747176def49
CyCklop <ignacioandresflores06@gmail.com>
2026-06-06 20:25:10 -0400

    terminado ejercicio de condicionales

---
+++ condicionales/condicionales.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // CONDICIONAL 'IF'
+    int autorizado = 1;
+
+    // Si 'autorizado' es 1, puede ingresar, sino, no puede
+    if (autorizado) {
+        printf("Puede ingresar\n");
+    } else {
+        printf("No puede ingresar\n");
+    }
+
+    // CONDICIONAL IF, ELSEIF
+    int entero = 100;
+
+    if (entero == 99) {
+        printf("Es 99\n");
+    } else if (entero == 100) {
+        printf("Es 100\n");
+    } else {
+        printf("No es ni 99, ni 100\n");
+    }
+
+    return 0;
+}
